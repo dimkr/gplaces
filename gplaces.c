4dfdbb850dac6e2a3642e453ef482c15d6cd53ca
Dima Krasner <dima@dimakrasner.com>
2022-05-07 18:08:31 +0300

    cleanup

--- gplaces.c
+++ gplaces.c
@@ -748,9 +748,8 @@ static SelectorList download_to_temp(Selector *sel, int ask, int gemtext) {
 		if (fwrite(buffer, 1, received, fp) != (size_t)received) goto out;
 		total += received;
 		if (!parse && total > 2048 && total - prog > total / 20) { fputc('.', stderr); prog = total; }
-		else if (parse && interactive) fprintf(stdout, "\33[2m%.*s\33[0m", received, buffer);
+		else if (parse && interactive) fprintf(stderr, "\33[2m%.*s\33[0m", received, buffer);
 		if (total > SIZE_MAX - sizeof(buffer)) goto out;
-		if (parse && interactive) fprintf(stderr, "\33[2m%.*s\33[0m", received, buffer);
 	}
 	if (received < 0 && ((err = SSL_get_error(ssl, received)) != SSL_ERROR_ZERO_RETURN)) { /* some servers seem to ignore this part of the specification (v0.16.1): "As per RFCs 5246 and 8446, Gemini servers MUST send a TLS `close_notify`" */
 		if (err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) error(0, "failed to download `%s`: cancelled", sel->url);
