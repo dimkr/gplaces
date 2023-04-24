d456e0e48603178df2568d5bcfcef1b4322b1378
Dima Krasner <dima@dimakrasner.com>
2023-04-24 09:12:34 +0300

    update copyright year [skip ci]

--- tcp.c
+++ tcp.c
@@ -2,7 +2,7 @@
 ================================================================================
 
 	gplaces - a simple terminal Gemini client
-    Copyright (C) 2022  Dima Krasner
+    Copyright (C) 2022, 2023  Dima Krasner
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
